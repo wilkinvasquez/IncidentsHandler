7f55b207c044434af8a36ed3afd79c0efb3a76a1
wvasquez <wvasquez@digepres.gob.do>
2021-11-18 14:44:05 -0400

    Configured.

--- API/IncidentsHandler.Services/DTOs/DepartmentDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using IncidentsHandler.Services.DTOs.BaseEntityDto;
-
-namespace IncidentsHandler.Services.DTOs
-{
-    public class DepartmentDto : EntityDto
-    {
-        public string Name { get; set; }
-    }
-}
---
+++ Web/Models/ISampleService.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+namespace Models
+{
+    [ServiceContract]
+    public interface ISampleService
+    {
+        [OperationContract]
+        string Test(string s);
+
+        [OperationContract]
+        void XmlMethod(System.Xml.Linq.XElement xml);
+
+        [OperationContract]
+        MyCustomModel TestCustomModel(MyCustomModel inputModel);
+    }
+}
