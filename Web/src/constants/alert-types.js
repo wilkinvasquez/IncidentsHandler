26031477f679daf7d4f7131c1f8271bd4c2d6aa4
wvasquez <wvasquez@digepres.gob.do>
2021-11-18 11:06:41 -0400

    Configured.

--- Web/src/constants/alert-types.js
+++
@@ -1,8 +0,0 @@
-const AlertTypes = {
-    SUCCESS: 1,
-    WARNNING: 2,
-    ERROR: 3,
-    CONFIRM: 4
-}
-
-export default AlertTypes;
--- Web/src/services/token-service.js
+++
@@ -1,9 +0,0 @@
-const getToken = () => {
-    return document.cookie.split(';').filter(element => element.includes('incidentsHandlerToken'))[0].split('=')[1];
-}
-
-const TokenService = {
-    getToken
-};
-
-export default TokenService;
